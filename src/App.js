3c76cf636640e1f79c3507cafed3036bab195ad5
stephenPC <stephenmissah@outlook.com>
2021-01-28 21:13:21 +0000

    got it working - just need styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React, { useEffect, useState } from "react";
 import RatesSelect from "./components/RatesSelect";
+import "./App.css";
 
 const App = () => {
   const [currencies, setCurrencies] = useState([]);
+  const [rate, setRate] = useState();
+  const [enteredNumber, setEnteredNumber] = useState();
+  const [result, setResult] = useState("");
+
+  const handleCurrencyPick = (currency) => {
+    setRate(currency);
+    console.log(`${rate} is the selected rate`);
+  };
+
+  const handleResult = () => {
+    const result = enteredNumber * rate;
+    console.log(result);
+    setResult(result);
+  };
+
+  useEffect(() => {
+    handleResult();
+    console.log("test");
+  }, [rate]);
 
   useEffect(() => {
     const getCurrencies = async () => {
       const currenciesFromApi = await fetchCurrencies();
       setCurrencies(currenciesFromApi);
+      console.log(currencies);
     };
     getCurrencies();
   }, []);
@@ -17,14 +38,35 @@ const App = () => {
 
     const result = await fetch(url);
     const data = await result.json();
-    console.log(data);
+    // console.log(data);
     return data;
   };
 
   return (
     <div>
       <h1>Currency Convertor</h1>
-      {currencies.length > 0 && <RatesSelect rates={currencies} />}
+      {currencies.length <= 0 ? (
+        <h1 className="loading"> Loading</h1>
+      ) : (
+        <RatesSelect
+          rates={currencies}
+          handleCurrencyPick={handleCurrencyPick}
+        />
+      )}
+
+      <input
+        type="number"
+        onChange={(e) => {
+          // if (typeof e.target.value === "number") {
+          //   alert("Number!!");
+          // }
+          setEnteredNumber(e.target.value);
+          handleResult();
+        }}
+      />
+      <div className="results">
+        {typeof result === "number" ? <h3>{result}</h3> : "sds"}
+      </div>
     </div>
   );
 };
--- src/components/RatesSelect.js
+++ src/components/RatesSelect.js
@@ -1,25 +1,32 @@
 import React from "react";
 
-const RatesSelect = ({ rates }) => {
-  //   const currencies = props.rates.rates;
-  //   console.log(props.rates.rates);
-  const currencies = rates[1];
-  console.log(currencies);
-  //   console.log(Object.entries(currencies));
+const RatesSelect = ({ rates, handleCurrencyPick }) => {
+  const currencies = rates.rates;
+  // console.log(currencies);
+
+  // console.log(Object.entries(currencies));
+
+  // const currencyObj = Object.entries(currencies);
 
   return (
     <div className="form">
-      <select name="" id="">
-        <option value="default" selected>
-          Please pick an option
+      {/* //!desctructed the object array thing */}
+      <select
+        onChange={(e) => {
+          // console.log(e.target.value);
+          handleCurrencyPick(e.target.value);
+        }}
+      >
+        <option value="default" defaultValue>
+          Please pick an currency
         </option>
-        {/* {Object.entries(currencies).map(([currency, rate], i) => {
+        {Object.entries(currencies).map(([currency, rate], index) => {
           return (
-            <option id={i} value={currency}>
+            <option key={index} value={rate}>
               {currency}
             </option>
           );
-        })} */}
+        })}
       </select>
     </div>
   );
